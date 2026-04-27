3de8700de07f12210ad31d34c987006a62458c41
xhvolkam <xhvolkam@stuba.sk>
2026-04-27 20:41:57 +0200

    Add files via upload

--- Firmware_Implementation/Motor_calibration/Motor_calibration.ino
+++ Firmware_Implementation/Motor_calibration/Motor_calibration.ino
@@ -1,1 +1,25 @@
+#include <ESP32Servo.h>
 
+Servo escLeft, escRight;
+
+const int ESC_LEFT_PIN = 17;
+const int ESC_RIGHT_PIN = 18;
+
+int PWM_TEST = 1190;
+int PWM_TEST_2 = 1160; 
+
+void setup() {
+  escLeft.attach(ESC_LEFT_PIN);
+  escRight.attach(ESC_RIGHT_PIN);
+
+  escLeft.writeMicroseconds(1090);
+  escRight.writeMicroseconds(1090);
+
+  delay(3000);
+
+  escLeft.writeMicroseconds(PWM_TEST);
+  escRight.writeMicroseconds(PWM_TEST_2);
+}
+
+void loop() {
+}
